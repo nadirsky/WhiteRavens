534a72d2a3d3cc91293099b781257f09a838d4f0
Nadirsky <sokolowski.maksymilian@gmail.com>
2016-10-27 12:49:18 +0200

    Fix gathering book info

--- GatheringData.sh
+++ GatheringData.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+GetBookInfo()
+{
+	#ISBN=$(grep -m 1 -E -o ".{0,0}978.{0,10}" $html)
+	title=$(grep -m 1 -E -o ".{0,0}<title>.{0,50}" $html | awk 'BEGIN{FS="-"} {print $1}' | awk 'BEGIN{FS=">"} {print $2}' | iconv -f utf-8 -t ascii//translit | sed 's/[!@#\$%^&*()'\'']//g')
+	author=$(grep -m 1 -E -o ".{0,0}SublineTags\">.{0,50}" $html | awk 'BEGIN{FS=">"} {print $2}' | awk 'BEGIN{FS=","} {print $1}' | iconv -f utf-8 -t ascii//translit | sed 's/[!@#\$%^&*()'\'']//g')
+	publishing=$(grep -m 1 -E -o ".{0,0}wydawnictwo: .{0,50}" $html | awk 'BEGIN{FS=": "} {print $2}' | awk 'BEGIN{FS="<"} {print $1}' | iconv -f utf-8 -t ascii//translit |sed 's/[!@#\$%^&*()'\'']//g')
+	binding=$(grep -m 1 -E -o ".{0,0}oprawa.{0,20}" $html | awk 'BEGIN{FS=","} {print $1}'| awk 'BEGIN{FS=" "} {print $2}' | iconv -f utf-8 -t ascii//translit | sed 's/[!@#\$%^&*()'\'']//g')
+	premiere=$(grep -m 1 -E -o ".{0,0}rok wydania.{0,8}" $html | awk 'BEGIN{FS=","} {print $1}' | awk 'BEGIN{FS=" "} {print $3}' | iconv -f utf-8 -t ascii//translit | sed 's/[!@#\$%^&*()'\'']//g')
+	echo $isbn $title $author $publishing $binding $premiere $address
+
+	mysql --defaults-extra-file=./mysql.cnf white_ravens -e "CREATE TABLE IF NOT EXISTS books(ISBN varchar(13) NOT NULL,Title tinytext,Author tinytext,Publishing tinytext,Binding tinytext,Premiere tinytext,Address tinytext,PRIMARY KEY (ISBN));"
+	mysql --defaults-extra-file=./mysql.cnf white_ravens -e "INSERT INTO books VALUES ($isbn, '$title', '$author', '$publishing', '$binding', '$premiere', '$address');"
+}
 
 GatheringData()
 {
@@ -20,26 +33,30 @@ GatheringData()
 
     while read line;
       do
-	adres=$(echo $line | awk '{print $2}') 
-	wget -q -O $html $adres
+	address=$(echo $line | awk '{print $2}') 
+	wget -q -O $html $address
         
-	#isbn=$(grep -m 1 -E -o ".{0,0}ISBN: .{0,13}" $html | awk '{print $2}')
-	isbn=$(grep -m 1 -E -o ".{0,0}978.{0,10}" $html)
-	date=$(grep -m 1 -E -o ".{0,0}now: new Date.{0,25}" $html | awk 'BEGIN{FS="("} {print $2}' | awk 'BEGIN{FS=","} {print $1, $2, $3, $4, $5, $6}')	
-	#price=$(grep -m 1 -E -o ".{0,0}\"LowestPrice\".{0,6}" $html | awk 'BEGIN{FS=":"} {print $2}' | awk '{sub(",", ""); print}')
-	#offers=$(grep -m 1 -E -o ".{0,0}\"OffersCount\".{0,4}" $html | awk '{sub(":", " "); sub(",", " "); print $2}')
+	#isbn=$(grep -m 1 -E -o ".{0,0}978.{0,10}" $html)
+	isbn=$(echo $line | awk '{print $1}')
+	date=$(grep -m 1 -E -o ".{0,0}now: new Date.{0,25}" $html | awk 'BEGIN{FS="("} {print $2}' | awk 'BEGIN{FS=","} {print $1, $2, $3, $4, $5, $6}')
+	date2=$(grep -m 1 -E -o ".{0,0}now: new Date.{0,25}" $html | awk 'BEGIN{FS="("} {print $2}' | awk 'BEGIN{FS=","} {print $1 "," $2 "," $3 "," $4 "," $5 "," $6}')		
 	price=$(grep -m 1 -E -o ".{0,0}minPrice\",\".{0,6}" $html | awk 'BEGIN{FS="\""} {print $3}')
 	offers=$(grep -m 1 -E -o ".{0,0}offersNo\",\".{0,3}" $html | awk 'BEGIN{FS="\""} {print $3}')
-	echo $date $offers $price >> Data/$isbn.dat
+	#echo $date $offers $price >> Data/$isbn.dat
 
-	if [ ! -e ./Data/$isbn.info ]; then
-		title=$(grep -m 1 -E -o ".{0,0}<title>.{0,50}" $html | awk 'BEGIN{FS="-"} {print $1}' | awk 'BEGIN{FS=">"} {print ";", $2, ";"}')
-		author=$(grep -m 1 -E -o ".{0,0}Autor: .{0,50}" $html | awk 'BEGIN{FS="\""} {print $1}' | awk 'BEGIN{FS=":"} {print $2, ";"}')
-		publishing=$(grep -m 1 -E -o ".{0,0}Wydawnictwo: .{0,50}" $html | awk 'BEGIN{FS="\""} {print $1}' | awk 'BEGIN{FS=":"} {print $2, ";"}')
-		binding=$(grep -m 1 -E -o ".{0,0}, oprawa.{0,20}" html.dat | awk 'BEGIN{FS=","} {print $2, ";"}')
-		premiere=$(grep -m 1 -E -o ".{0,0}Created.{0,10}" $html | awk 'BEGIN{FS=":"} {print $2}' | awk 'BEGIN{FS=","} {print $1}')
-        	echo $isbn $adres $title $author $publishing $binding $premiere > Data/$isbn.info
-	fi
+	mysql --defaults-extra-file=./mysql.cnf white_ravens -e "CREATE TABLE IF NOT EXISTS book$isbn (Year int, Month int, Day int, Hour int, Minute int, Second int, Offers int, MinPrice float);"
+	mysql --defaults-extra-file=./mysql.cnf white_ravens -e "INSERT INTO book$isbn VALUES ($date2,$offers,$price);"
+
+	GetBookInfo
+
+	#if [ ! -e ./Data/$isbn.info ]; then
+	#	title=$(grep -m 1 -E -o ".{0,0}<title>.{0,50}" $html | awk 'BEGIN{FS="-"} {print $1}' | awk 'BEGIN{FS=">"} {print $2}')
+	#	author=$(grep -m 1 -E -o ".{0,0}SublineTags\">.{0,50}" $html | awk 'BEGIN{FS=">"} {print $2}' | awk 'BEGIN{FS=","} {print $1}')
+	#	publishing=$(grep -m 1 -E -o ".{0,0}wydawnictwo: .{0,50}" $html | awk 'BEGIN{FS=": "} {print $2}' | awk 'BEGIN{FS="<"} {print $1}')
+	#	binding=$(grep -m 1 -E -o ".{0,0}oprawa.{0,20}" $html | awk 'BEGIN{FS=","} {print $1}'| awk 'BEGIN{FS=" "} {print $2}')
+	#	premiere=$(grep -m 1 -E -o ".{0,0}rok wydania.{0,8}" $html | awk 'BEGIN{FS=","} {print $1}' | awk 'BEGIN{FS=" "} {print $3}')
+        #	echo $isbn $address $title $author $publishing $binding $premiere > Data/$isbn.info
+	#fi
 
 	if [ ! -e ./Data/$isbn.allegro ]; then
 		title=$(grep -m 1 -E -o ".{0,0}<title>.{0,50}" $html | awk 'BEGIN{FS="-"} {print $1}' | awk 'BEGIN{FS=">"} {print $2}' | awk '{gsub(/ /,"+")} {print}' )
