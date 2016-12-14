4e150fdbfc21b26f0b885fe6a8171fb7fd4bec9c
Nadirsky <sokolowski.maksymilian@gmail.com>
2016-12-14 08:24:21 +0100

    Mysql dump

--- Cron.sh
+++ Cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=/home/nadirsky/Desktop/Books:$PATH
+PATH=/home/nadirsky/Desktop/Books:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/nadirsky/anaconda3/bin:/usr/bin/python
 
 cd /home/nadirsky/Desktop/Books
 make all
--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: gatherData analize
+all: gatherData analize dump
 
 gatherData:
 	python gather.py
 
 analize:
 	./Analize.sh
+
+dump:
+	./Mysqldump.sh
